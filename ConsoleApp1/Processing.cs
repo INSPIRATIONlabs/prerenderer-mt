3ff5976f6883cd218656fb9fbfac8ef8dea732ed
Dominic Böttger <dominic.boettger@inspirationlabs.com>
2018-10-25 10:41:50 +0200

    init

---
+++ ConsoleApp1/Processing.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace com.inspirationlabs.prerenderer
+{
+    class Processing
+    {
+        const int MAX_PROCESSORS = 100;
+        SemaphoreSlim _semaphore = new SemaphoreSlim(MAX_PROCESSORS);
+        HashSet<Task> _pending = new HashSet<Task>();
+        object _lock = new Object();
+
+        async Task ProcessAsync(string data)
+        {
+            await _semaphore.WaitAsync();
+            try
+            {
+                await Task.Run(() =>
+                {
+                    // simuate work
+                    //Thread.Sleep(1000);
+                    Console.WriteLine("done");
+                });
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        public async void QueueItemAsync(string data)
+        {
+            var task = ProcessAsync(data);
+            lock (_lock)
+                _pending.Add(task);
+            try
+            {
+                await task;
+            }
+            catch
+            {
+                if (!task.IsCanceled && !task.IsFaulted)
+                    throw; // not the task's exception, rethrow
+                           // don't remove faulted/cancelled tasks from the list
+                return;
+            }
+            // remove successfully completed tasks from the list 
+            lock (_lock)
+                _pending.Remove(task);
+        }
+
+        public async Task WaitForCompleteAsync()
+        {
+            Task[] tasks;
+            lock (_lock)
+                tasks = System.Linq.Enumerable.ToArray(_pending);
+            await Task.WhenAll(tasks);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using PuppeteerSharp;
+
+namespace com.inspirationlabs.prerenderer
+{
+    class Prerenderer
+    {
+        static void Main(string[] args)
+        {
+            // wait for MainTask (async)
+            Maintask().Wait();
+        }
+
+        static async Task Maintask()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync("https://api.staging.mydriver-international.com/mydriver-cms/v3/cms/url");
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                JObject jObject = JObject.Parse(responseBody);
+                JArray urldata = (JArray)jObject["data"];
+
+                var fetcher = await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+                await DownloadAsync(urldata);
+            } catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        const int MAX_DOWNLOADS = 50;
+
+        // download data
+        static async Task DownloadAsync(JArray urls)
+        {
+            int count = 0;
+            Processing processing = new Processing();
+
+            using (SemaphoreSlim semaphore = new SemaphoreSlim(MAX_DOWNLOADS))
+            using (Browser browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = false,
+                Args =  new[] { "--no-sandbox", "--disable-setuid-sandbox" }
+            }))
+            {
+                var tasks = urls.Select(async (urldata) =>
+                {
+                    await semaphore.WaitAsync();
+                    try
+                    {
+                        count++;
+                        Page page = await browser.NewPageAsync();
+                        page.DefaultNavigationTimeout = 120000;
+                        var setIsServer = @"
+                            Object.defineProperty(window, 'isServer', {
+                                get() {
+                                    return true
+                                }
+                            });
+                        ";
+                        await page.EvaluateOnNewDocumentAsync(setIsServer);
+                        await page.SetRequestInterceptionAsync(true);
+                        page.Request += (sender, e) =>
+                        {
+                            string resType = e.Request.ResourceType.ToString();
+                            if (resType == "Image" || resType == "Font")
+                            {
+                                e.Request.AbortAsync();
+                            } else
+                            {
+                                e.Request.ContinueAsync();
+                            }
+                        };
+                        string path = (string)urldata.SelectToken("url");
+                        string url = "http://localhost:2015" + path;
+                        await page.GoToAsync(url, new NavigationOptions { WaitUntil = new[] { WaitUntilNavigation.Networkidle0 } });
+                        string content = await page.GetContentAsync();
+                        await page.CloseAsync();
+                        Console.WriteLine(count);
+                        // put the result on the processing pipeline
+                        // processing.QueueItemAsync(content);
+                    }
+                    finally
+                    {
+
+                        semaphore.Release();
+                    }
+                });
+
+                await Task.WhenAll(tasks.ToArray());
+                // await processing.WaitForCompleteAsync();
+            }
+        }
+    }
+}
