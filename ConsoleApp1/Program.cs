871a1893d7bd9efb4e8c442b8199fe642ee0ca92
Dominic Böttger <dominic.boettger@inspirationlabs.com>
2018-10-26 15:07:47 +0200

    Added file handling and arguments

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,19 +1,50 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using NDesk.Options;
 using PuppeteerSharp;
+using System.Reflection;
 
 namespace com.inspirationlabs.prerenderer
 {
     class Prerenderer
     {
+        static string Host = "http://127.0.0.1:2015";
+        static int Threads = 50;
+        static string Jsonurl = "https://api.staging.mydriver-international.com/mydriver-cms/v3/cms/url";
+        static string OutputPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "/output";
+        static DirectoryInfo Cwd;
         static void Main(string[] args)
         {
+            var p = new OptionSet() {
+               { "host=", "Set the hostname", v => Host = v },
+               { "threads=", "Set the amount of paralell threads", (int v) => Threads = v },
+               { "jsonurl=", "Set the endpoint url to get the url list", v => Jsonurl = v},
+               { "outputpath=", "Set the path to output the contents", v => OutputPath = v }
+            };
+            List<string> extra = p.Parse(args);
+            
+            try
+            {
+                // delete outputpath if it exists
+                if(OutputPath.Length > 0 && Directory.Exists(OutputPath))
+                {
+                    Directory.Delete(OutputPath, true);
+                }
+                if(OutputPath.Length > 0)
+                {
+                    Console.WriteLine("Creating outputpath " + OutputPath);
+                    Cwd = Directory.CreateDirectory(OutputPath);
+                }
+            } catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
             // wait for MainTask (async)
             Maintask().Wait();
         }
@@ -23,7 +54,7 @@ namespace com.inspirationlabs.prerenderer
             try
             {
                 HttpClient client = new HttpClient();
-                HttpResponseMessage response = await client.GetAsync("https://api.staging.mydriver-international.com/mydriver-cms/v3/cms/url");
+                HttpResponseMessage response = await client.GetAsync(Jsonurl);
                 response.EnsureSuccessStatusCode();
 
                 string responseBody = await response.Content.ReadAsStringAsync();
@@ -37,16 +68,14 @@ namespace com.inspirationlabs.prerenderer
                 Console.WriteLine(e.Message);
             }
         }
-
-        const int MAX_DOWNLOADS = 50;
-
+        
         // download data
         static async Task DownloadAsync(JArray urls)
         {
             int count = 0;
             Processing processing = new Processing();
 
-            using (SemaphoreSlim semaphore = new SemaphoreSlim(MAX_DOWNLOADS))
+            using (SemaphoreSlim semaphore = new SemaphoreSlim(Threads))
             using (Browser browser = await Puppeteer.LaunchAsync(new LaunchOptions
             {
                 Headless = false,
@@ -82,10 +111,15 @@ namespace com.inspirationlabs.prerenderer
                             }
                         };
                         string path = (string)urldata.SelectToken("url");
-                        string url = "http://localhost:2015" + path;
+                        string url = Host + path;
                         await page.GoToAsync(url, new NavigationOptions { WaitUntil = new[] { WaitUntilNavigation.Networkidle0 } });
                         string content = await page.GetContentAsync();
+                        
                         await page.CloseAsync();
+                        using (StreamWriter outputFile = new StreamWriter(Path.Combine(Path.Combine(OutputPath, url), "/index.html")))
+                        {
+                            await outputFile.WriteAsync(content);
+                        }
                         Console.WriteLine(count);
                         // put the result on the processing pipeline
                         // processing.QueueItemAsync(content);
