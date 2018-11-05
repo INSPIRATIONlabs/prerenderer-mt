23b43fd2230f5616032f22c8af8875c3f1fc823d
DanielWalther <daniel.walther@inspirationlabs.com>
2018-11-05 18:24:53 +0100

    adding ssr, ssrv & ssrc w/o shadow or depth check

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -28,20 +28,21 @@ namespace com.inspirationlabs.prerenderer
                { "outputpath=", "Set the path to output the contents", v => OutputPath = v }
             };
             List<string> extra = p.Parse(args);
-            
+
             try
             {
                 // delete outputpath if it exists
-                if(OutputPath.Length > 0 && Directory.Exists(OutputPath))
+                if (OutputPath.Length > 0 && Directory.Exists(OutputPath))
                 {
                     Directory.Delete(OutputPath, true);
                 }
-                if(OutputPath.Length > 0)
+                if (OutputPath.Length > 0)
                 {
                     Console.WriteLine("Creating outputpath " + OutputPath);
                     Cwd = Directory.CreateDirectory(OutputPath);
                 }
-            } catch(Exception e)
+            }
+            catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
@@ -60,15 +61,29 @@ namespace com.inspirationlabs.prerenderer
                 string responseBody = await response.Content.ReadAsStringAsync();
                 JObject jObject = JObject.Parse(responseBody);
                 JArray urldata = (JArray)jObject["data"];
+                urldata.AddFirst(JToken.Parse("{\"url\": \"/\", \"published\":true, \"indexed\":true,\"followed\":true}"));
+                urldata.AddFirst(JToken.Parse("{\"url\": \"/de\", \"published\":true, \"indexed\":true,\"followed\":true}"));
+                urldata.AddFirst(JToken.Parse("{\"url\": \"/en\", \"published\":true, \"indexed\":true,\"followed\":true}"));
+                urldata.AddFirst(JToken.Parse("{\"url\": \"/es\", \"published\":true, \"indexed\":true,\"followed\":true}"));
+                urldata.AddFirst(JToken.Parse("{\"url\": \"/it\", \"published\":true, \"indexed\":true,\"followed\":true}"));
+                urldata.AddFirst(JToken.Parse("{\"url\": \"/nl\", \"published\":true, \"indexed\":true,\"followed\":true}"));
+                urldata.AddFirst(JToken.Parse("{\"url\": \"/fr\", \"published\":true, \"indexed\":true,\"followed\":true}"));
+
+                // testing
+                while (urldata.Count >= 200)
+                {
+                    urldata.Remove(urldata.Last);
+                }
 
                 var fetcher = await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
                 await DownloadAsync(urldata);
-            } catch(Exception e)
+            }
+            catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
         }
-        
+
         // download data
         static async Task DownloadAsync(JArray urls)
         {
@@ -87,7 +102,7 @@ namespace com.inspirationlabs.prerenderer
                     for (int i = 0; i <= Threads + 50; i++)
                     {
                         Page page = await browser.NewPageAsync();
-                        page.DefaultNavigationTimeout = 120000;
+                        page.DefaultNavigationTimeout = 150000;
                         var setIsServer = @"
                             function(){
                                 Object.defineProperty(window, 'isServer', {
@@ -98,30 +113,39 @@ namespace com.inspirationlabs.prerenderer
                             }
                         ";
                         await page.EvaluateOnNewDocumentAsync(setIsServer);
-                       
+
                         qt.Enqueue(page);
                     }
 
+                    String scriptBody = await File.ReadAllTextAsync("assets/prerender.js");
                     var tasks = urls.Select(async (urldata) =>
                     {
-                            await semaphore.WaitAsync();
-                            Page page = qt.Dequeue();
-                            try
-                            {
-                                
-                                string path = (string)urldata.SelectToken("url");
-                                string url = Host + path;
-                                await page.GoToAsync(url, new NavigationOptions { WaitUntil = new[] { WaitUntilNavigation.Networkidle0 } });
-                                string content = await page.GetContentAsync();
+                        await semaphore.WaitAsync();
+                        Page page = qt.Dequeue();
+                        try
+                        {
+                            string path = (string)urldata.SelectToken("url");
+                            string url = Host + path;
 
-                                // put the result on the processing pipeline
-                                processing.QueueItemAsync(content, path, OutputPath);
-                            }
-                            finally
-                            {
-                                qt.Enqueue(page);
-                                semaphore.Release();
-                            }
+                            await page.GoToAsync(url, new NavigationOptions { WaitUntil = new[] { WaitUntilNavigation.Networkidle0 } });
+
+                            await page.MainFrame.EvaluateFunctionAsync(@"function(){"
+                            + scriptBody
+                            + "}");
+                            string content = await page.GetContentAsync();
+
+                            // put the result on the processing pipeline
+                            processing.QueueItemAsync(content, path, OutputPath);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        finally
+                        {
+                            qt.Enqueue(page);
+                            semaphore.Release();
+                        }
 
                     });
 
@@ -129,7 +153,8 @@ namespace com.inspirationlabs.prerenderer
                     await processing.WaitForCompleteAsync();
                     await browser.CloseAsync();
                 }
-            } catch(Exception e)
+            }
+            catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
